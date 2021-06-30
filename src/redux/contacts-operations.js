d3a6f5f2b4e7ca5659b9e195dcc80b530b2668c2
Luhivskyi <luhivskyi.d@ukr.net>
2021-06-30 21:45:35 +0300

    final HW 7

--- src/redux/contacts-operations.js
+++ src/redux/contacts-operations.js
@@ -17,35 +17,31 @@ const fetchContacts = () => async dispatch => {
   }
 };
 
-// const fetchContacts = () => dispatch => {
-//   dispatch(actions.fetchContactsRequest());
-
-//   axios
-//     .get('/contacts')
-//     .then(({ data }) => dispatch(actions.fetchContactsSuccess(data)))
-//     .catch(error => actions.fetchContactsError(error));
-// };
-
-const addContact = (name, number) => dispatch => {
+const addContact = (name, number) => async dispatch => {
   const contact = {
     name,
     number,
   };
+
   dispatch(actions.addContactRequest());
 
-  axios
-    .post('/contacts', contact)
-    .then(({ data }) => dispatch(actions.addContactSuccess(data)))
-    .catch(error => dispatch(actions.addContactError(error)));
+  try {
+    const { data } = await axios.post('/contacts', contact);
+    dispatch(actions.addContactSuccess(data));
+  } catch (error) {
+    dispatch(actions.addContactError(error.message));
+  }
 };
 
-const deleteContact = contactId => dispatch => {
+const deleteContact = contactId => async dispatch => {
   dispatch(actions.deleteContactRequest());
 
-  axios
-    .delete(`/contacts/${contactId}`)
-    .then(() => dispatch(actions.deleteContactSuccess(contactId)))
-    .catch(error => dispatch(actions.deleteContactError(error)));
+  try {
+    await axios.delete(`/contacts/${contactId}`);
+    dispatch(actions.deleteContactSuccess(contactId));
+  } catch (error) {
+    dispatch(actions.deleteContactError(error.message));
+  }
 };
 
 export default {
